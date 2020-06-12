d79bdf9bc70b0b2c1772e9b08c8323174aef693f
Garvit Saxena <garvit.saxena@scool.in>
2020-06-12 13:50:01 +0530

    Updates

---
+++ precache-manifest.9518f2a3396d478e2aa27a3d9009d0b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "222ffbd88fb332e0dfd7c0c140c231e4",
+    "url": "/CRUD-application/index.html"
+  },
+  {
+    "revision": "107bba600db99f99356b",
+    "url": "/CRUD-application/static/css/main.7bedda0a.chunk.css"
+  },
+  {
+    "revision": "eafb951a00f8ddebf109",
+    "url": "/CRUD-application/static/js/2.d8137324.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CRUD-application/static/js/2.d8137324.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "107bba600db99f99356b",
+    "url": "/CRUD-application/static/js/main.8840d8d5.chunk.js"
+  },
+  {
+    "revision": "d0959d6ab3bd86439864",
+    "url": "/CRUD-application/static/js/runtime-main.e253cad2.js"
+  }
+]);
